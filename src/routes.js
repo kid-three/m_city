4870e7a64b3a38e6448452fe30d05ad7e40089de
Kinich Barcelo <kinich@kbdreams.com>
2019-04-28 13:14:49 -0500

    Finished layout, header and footer

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from './Hoc/Layout'
+import {Switch, Route} from 'react-router-dom';
+
+const Routes = (props) => {
+  return(
+    <Layout>
+      <Switch>
+        
+
+      </Switch>
+    </Layout>
+  )
+}
+
+export default Routes;
